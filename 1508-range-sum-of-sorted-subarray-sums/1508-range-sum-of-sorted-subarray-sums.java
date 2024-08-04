ca516edbd993a87b62a6ba8bc7e75a34a3b7426e
Nishant <61405271+152001@users.noreply.github.com>
2024-08-04 18:16:03 +0530

    Time: 65 ms (72.52%), Space: 46.6 MB (46.40%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -1,28 +1,19 @@
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
-         final int MOD = 1000000007;
-        
-        int[] prefix = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + nums[i];
-        }
-        
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                minHeap.offer(prefix[i] - prefix[j]);
+        int[] arr = new int[n * (n + 1) / 2];
+        for (int i = 0, k = 0; i < n; ++i) {
+            int s = 0;
+            for (int j = i; j < n; ++j) {
+                s += nums[j];
+                arr[k++] = s;
             }
         }
-        
-        long result = 0;
-        for (int i = 1; i <= right; i++) {
-            int sum_value = minHeap.poll();
-            if (i >= left) {
-                result = (result + sum_value) % MOD;
-            }
+        Arrays.sort(arr);
+        int ans = 0;
+        final int mod = (int) 1e9 + 7;
+        for (int i = left - 1; i < right; ++i) {
+            ans = (ans + arr[i]) % mod;
         }
-        
-        return (int) result;
+        return ans;
     }
 }
